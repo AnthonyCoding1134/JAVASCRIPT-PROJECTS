161dfcb3cd2370c94df9c10e943b77888744b04b
AnthonyCoding1134 <155090896+AnthonyCoding1134@users.noreply.github.com>
2024-12-17 11:24:41 -0500

    JS Basics

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,38 @@
+// window.alert("HI");
+
+// document.write("Hellooo World" + "\n" );
+// console.log("\n");
+
+
+
+// var a = "how are you feeling"; 
+// document.write(a);
+
+
+
+
+// var myString = "I'm hungry";
+
+// window.alert(myString);
+
+// document.write("i said to the barista \"One large iced coffee with extra cream and sugar\" and he made it perfectly");
+
+// document.write("i said to the end user \"Open your web browser and clear your cache" + " reboot your pc\" and this fixed the issue");
+
+// var myVar = "anthony is " + "the best!";
+
+//document.write(myVar);
+
+var anthony = "the youngest", susan = "the oldest", mom = "the best";
+//document.write(mom);
+
+
+myNum = 25 + 5;
+// document.write(myNum);
+
+
+var sent1 = "This is the beginning of the string";
+var sent2 = " and this is the end of the string";
+
+var fullSentence = sent1 + sent2;
+document.write(fullSentence);
